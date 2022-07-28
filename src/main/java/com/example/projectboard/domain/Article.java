950935643d8f1a0d674c4ac5b2af4810008f8fdb
seongsu.kim <tjdtn94k@naver.com>
2022-07-28 16:39:15 +0900

    backup

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -1,15 +1,77 @@
 package com.example.projectboard.domain;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+@Getter
+@ToString
+@Table(indexes = {
+    @Index(columnList = "title"),
+    @Index(columnList = "hashtag"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+
+@Entity
 public class Article {
-  private Long id;
-  private String title; // 제목
-  private String content; // 내용
-  private String hashtag; // 해시태그
 
-  private LocalDateTime createdAt; // 생성일시
-  private String createBy; // 생성자
-  private LocalDateTime modifiedAt; // 수정일시
-  private String modifiedBy; // 수정자
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Setter
+  @Column(nullable = false)
+  private String title; // 제목
+
+  @Setter @Column(nullable = false , length = 10000) private String content; // 내용
+  @Setter private String hashtag; // 해시태그
+
+  //jpa auditing 기능
+  @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+  @CreatedBy @Column(nullable = false , length = 100) private String createdBy; // 생성자
+  @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+  @LastModifiedBy @Column(nullable = false , length = 100) private String modifiedBy; // 수정자
+
+  protected Article(){}
+
+  private Article(String title, String content, String hashtag) {
+    this.title = title;
+    this.content = content;
+    this.hashtag = hashtag;
+  }
+
+  public static Article of(String title, String content, String hashtag) {
+    return new Article(title,content,hashtag);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Article article = (Article) o;
+    return id != null && id.equals(article.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
--- src/main/java/com/example/projectboard/domain/ArticleComment.java
+++ src/main/java/com/example/projectboard/domain/ArticleComment.java
@@ -1,14 +1,48 @@
 package com.example.projectboard.domain;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+@Getter
+@ToString
+@Table(indexes = {
+    @Index(columnList = "conte"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+
+@Entity
 public class ArticleComment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private Article article; // 게시글 id
-  private String content; // 내용
 
-  private LocalDateTime createdAt; // 생성일시
-  private String createBy; // 생성자
-  private LocalDateTime modifiedAt; // 수정일시
-  private String modifiedBy; // 수정자
+  @Setter @ManyToOne(optional = false) private Article article; // 게시글 id
+  @Setter @Column(nullable = false, length = 2000) private String content; // 내용
+
+  @CreatedDate
+  @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+  @CreatedBy
+  @Column(nullable = false , length = 100) private String createdBy; // 생성자
+  @LastModifiedDate
+  @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+  @LastModifiedBy
+  @Column(nullable = false , length = 100) private String modifiedBy; // 수정자
+
+
+
 }
